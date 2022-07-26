8d5aad68504ca626a44344e88b38041433bc4bbb
pablo <taddeiApablo23@gmail.com>
2022-07-26 11:37:30 -0300

    cambio de stock
    cambio en los precios unitarios de stock como ultima prueba

--- QuarkCotizador/QuarkCotizador/modelo/CotizadorModel.cs
+++ QuarkCotizador/QuarkCotizador/modelo/CotizadorModel.cs
@@ -73,21 +73,21 @@ namespace QuarkCotizador.modelo
         {
             List<Prenda> listado_prendas = new List<Prenda>();
             //500 camisas manga corta
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_corta, 1000, 100, Calidad.Standard));
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_corta, 1100, 100, Calidad.Premium));
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_corta, 900, 150, Calidad.Standard));
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_corta, 1000, 150, Calidad.Premium));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_corta, 100, 100, Calidad.Standard));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_corta, 100, 100, Calidad.Premium));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_corta, 100, 150, Calidad.Standard));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_corta, 100, 150, Calidad.Premium));
             // 500 camisas manga larga
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_larga, 1000, 75, Calidad.Standard));
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_larga, 1900, 75, Calidad.Premium));
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_larga, 1000, 175, Calidad.Standard));
-            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_larga, 1900, 175, Calidad.Premium));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_larga, 100, 75, Calidad.Standard));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_mao, TipoPrenda.Manga_larga, 100, 75, Calidad.Premium));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_larga, 100, 175, Calidad.Standard));
+            listado_prendas.Add(new Camisa(TipoPrenda.Cuello_comun, TipoPrenda.Manga_larga, 100, 175, Calidad.Premium));
             // 1500 pantalones chupines
-            listado_prendas.Add(new Pantalon(TipoPrenda.Chupin, Calidad.Standard, 1000, 750));
-            listado_prendas.Add(new Pantalon(TipoPrenda.Chupin, Calidad.Premium, 1900, 750));
+            listado_prendas.Add(new Pantalon(TipoPrenda.Chupin, Calidad.Standard, 100, 750));
+            listado_prendas.Add(new Pantalon(TipoPrenda.Chupin, Calidad.Premium, 100, 750));
             // 500 pantalones comunes
-            listado_prendas.Add(new Pantalon(TipoPrenda.Comun, Calidad.Standard, 950, 250));
-            listado_prendas.Add(new Pantalon(TipoPrenda.Comun, Calidad.Premium, 1100, 250));
+            listado_prendas.Add(new Pantalon(TipoPrenda.Comun, Calidad.Standard, 100, 250));
+            listado_prendas.Add(new Pantalon(TipoPrenda.Comun, Calidad.Premium, 100, 250));
             return listado_prendas;
         }
         public (int, double) modelfiltroCamisas(int tipoManga, int tipoCuello, int calidad)
